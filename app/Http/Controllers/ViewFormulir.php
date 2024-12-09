cb912db214cedc590217e431bf4bc3557d0a8f75
turrud <fatur14022@gmail.com>
2024-12-10 03:30:57 +0700

    tet

--- app/Http/Controllers/ViewFormulir.php
+++ app/Http/Controllers/ViewFormulir.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Formulir;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ViewFormulir extends Controller
 {
--- database/migrations/2024_12_09_180717_001_create_Formulir_table.php
+++ database/migrations/2024_12_09_180717_001_create_Formulir_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('Formulir', function (Blueprint $table) {
+        Schema::create('formulir', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('phone');
@@ -42,6 +42,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('Formulir');
+        Schema::dropIfExists('formulir');
     }
 };
